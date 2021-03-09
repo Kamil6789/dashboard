629b6edb76a1c1ea5a778cf98568f065afa65ac7
Kamil <kamilsajnog@gmail.com>
2021-03-09 15:22:33 +0100

    Initialized new components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
     <div id="app">
-
+        <Navbar />
+        <Weather />
+        <Todo />
+        <Footer />
     </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+import Weather from './components/Weather.vue'
+import Todo from './components/Todo.vue'
+import Footer from './components/Footer.vue'
+
 export default {
     name: 'Dashboard',
     components: {
-
+        Navbar,
+        Weather,
+        Todo,
+        Footer
     }
 }
 </script>
