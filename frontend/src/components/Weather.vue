b03ba3af6d7b1ed586a1e8b2379a211b4cb0504b
Kamil <kamilsajnog@gmail.com>
2021-03-10 10:19:17 +0100

    Centering in weather container, added test image background

--- frontend/src/components/Weather.vue
+++ frontend/src/components/Weather.vue
@@ -1,6 +1,12 @@
 <template>
     <div id="weather-container">
-        <h1>Weather</h1>
+        <div class="container h-100">
+            <div class="row h-100 justify-content-center align-items-center">
+                <form class="col-12">
+                    <h1 class="text-center">Weather</h1>
+                </form>   
+            </div>
+        </div>
     </div>
 </template>
 
