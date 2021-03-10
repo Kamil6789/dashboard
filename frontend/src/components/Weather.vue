01e26d84c018dca7b4832e27b283e6c771cc671b
Kamil <kamilsajnog@gmail.com>
2021-03-10 12:48:58 +0100

    Added form, form handling, loader (spinner)

--- frontend/src/components/Weather.vue
+++ frontend/src/components/Weather.vue
@@ -2,18 +2,54 @@
     <div id="weather-container">
         <div class="container h-100">
             <div class="row h-100 justify-content-center align-items-center">
-                <form class="col-12">
+                <div class="col-12">
                     <h1 class="text-center">Weather</h1>
-                </form>   
+                    <form @submit.prevent="onSubmit">
+                        <input type="text" name="search" v-model="search" class="form-control search" placeholder="Wpisz nazwę miejscowości" required />
+                        <button type="sumbit" class="btn btn-primary">Wyszukaj</button>
+                    </form>
+                    <div v-if="loading">
+                        <Loader class="loader mt-4" color="#ffffffca" size="260" sizeUnit="px" />
+                    </div>
+                    <div class="mt-4" v-else>
+                        <div v-if="(data !== 'none')">
+                            <p>1</p>
+                        </div>
+                    </div>
+                </div>   
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import {RingLoader} from '@saeris/vue-spinners'
+
 import '../css/weather.css'
 
 export default {
-    name: 'Weather'
+    name: 'Weather',
+    components: {
+        'Loader': RingLoader
+    },
+    methods: {
+        onSubmit: async function () {
+            this.loading = true;
+            try {
+                this.loading = false;
+                this.data = null;
+            } catch(err) {
+                this.data = null
+            }
+        }
+    },
+    data() {
+        return {
+            loading: false,
+            search: this.search,
+            data: "none"
+        };
+    }
 }
+
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {VueSpinners} from '@saeris/vue-spinners'
 
 Vue.config.productionTip = false
+Vue.use(VueSpinners)
 
 new Vue({
     render: h => h(App),
